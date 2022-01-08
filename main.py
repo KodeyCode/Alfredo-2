8a64f30a41a3a865a2080e77ad5ba68e86975b0e
KodeyCode <81772857+KodeyCode@users.noreply.github.com>
2022-01-08 09:56:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,95 @@
+from sys import exit,argv
+f = open(argv[1])
+var = {}
+lineNum = 0
+pos=0
+for line in f:
+    line = line.replace('\t','')
+    line = line.replace('\n','')
+    lineNum+=1
+    if '\t' in line:
+        pos+=1
+    elif '+' in line:
+        a = int(line.split('+')[0])
+        b = int(line.split('+')[1])
+        op = a+b
+        print(op)
+    elif '*' in line:
+        a = int(line.split('*')[0])
+        b = int(line.split('*')[1])
+        op = a*b
+        print(op)
+    elif '-' in line:
+        a = int(line.split('-')[0])
+        b = int(line.split('-')[1])
+        op = a-b
+        print(op)
+    elif '/' in line:
+        a = int(line.split('/')[0])
+        b = int(line.split('/')[1])
+        op = a/b
+        print(op)
+    elif '=' in line:
+        if '?' in line:
+            var.update({line.split('=')[0]:input(line.split('?')[1])})
+            var.update({str(line.split('=')[0])+'.type':'<input>'})
+        elif '"' in line.split('=')[1]:
+            var.update({line.split('=')[0]:str(line.split('"')[1])})
+            var.update({str(line.split('=')[0])+'.type':'<string>'})
+        elif ':' in line.split('=')[1]:
+            var.update({line.split('=')[0]:int(line.split(':')[1])})
+            var.update({str(line.split('=')[0])+'.type':'<integer>'})
+    elif 'out' in line:
+        if '"' in line.split(' ')[1]:
+            print(line.split('"')[1])
+        else:
+            try:
+                print(var[line.split(' ')[1]])
+            except:
+                print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nVariable "'+line.split(' ')[1]+'" is not existent.')
+                exit()
+    elif 'read' in line:
+        if '"' in line.split(' ')[1]:
+            try:
+                f = open(line.split('"')[1],'r')
+                print(f.read())
+            except:
+                print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nFile "'+line.split('"')[1]+'" is either non existent or not accessible.')
+                exit()
+        else:
+            try:
+                f = open(var[line.split(' ')[1]])
+                print(f.read())
+            except:
+                print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nFile "'+var[line.split(' ')[1]]+'" is either non existent or not accessible.')
+                exit()
+    elif 'append' in line:
+        if '"' in line.split(' ')[1]:
+            try:
+                f = open(line.split('"')[1],'a')
+                if '"' in line.split(' ')[1]:
+                    f.write(str(line.split('"')[2]).replace(' ','',1))
+                else:
+                    f.write(var[line.split('"')[1]])
+            except:
+                print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nFile "'+line.split('"')[1]+'" is either non existent or not accessible.')
+                exit()
+        else:
+            try:
+                f = open(var[line.split(' ')[1]],'a')
+                if '"' in line.split(' ')[2]:
+                    f.write('\n')
+                    f.write(line.split('"')[2])
+                else:
+                    f.write('\n')
+                    f.write(var[line.split('"')[1]])
+            except:
+                print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nFile "'+var[line.split(' ')[1]]+'" is either non existent or not accessible.')
+                exit()
+    elif line.startswith('::'):
+        pass
+    elif line == 'debug':
+        print('Variables: '+str(var)+',\nFile: '+f.name+',\nLine Number: '+str(lineNum)+',\n')
+    else:
+        print('Error at "'+f.name+'" line '+str(lineNum)+':\n   '+line+'\nCommand "'+line.split(' ')[0]+'" not identified')
+        exit()
